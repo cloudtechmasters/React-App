342363ab31accb1a79c948c68e21c9ed23efb967
Sathish M <sathish@Apples-MacBook-Pro.local>
2020-07-31 22:58:57 +0530

    initial-commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:12.2.0-alpine as react_build 
+#also say 
+WORKDIR /app
+#copy the react app to the container
+COPY . /app/ 
+
+# #prepare the contiainer for building react 
+RUN npm install
+# RUN npm install react-scripts@3.0.1 -g --silent 
+RUN npm run build 
+
+#prepare nginx
+FROM nginx:1.16.0-alpine
+
+COPY --from=react_build /app/build /usr/share/nginx/html
+RUN rm /etc/nginx/conf.d/default.conf
+COPY nginx/nginx.conf /etc/nginx/conf.d
+
+
+
+#fire up nginx
+EXPOSE 80 
+CMD ["nginx","-g","daemon off;"]
--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [countryData, setCountryData] = useState([]);
+
+const [districData, setDistrictData] = useState([]);
+
+useEffect(() => {
+  // const apiUrl = `http://localhost:8080`;
+  // fetch(apiUrl)
+  //   .then((res) => res.json())
+  //   .then((repos) => {
+  //     setCountryData(repos);
+  //   });
+}, []);
+
+
+const getCountryData = async () => {
+
+  const result =  await axios(
+    'http://localhost:8080/api1',
+  );
+  setCountryData(result.data);  
+}
+
+const getDistrictData = async () => {
+
+  const result =  await axios(
+    'http://localhost:3000/api2',
+  );
+  setDistrictData(result.data);  
+}
   return (
     <div className="App">
+      <div className="header-bar">REACT - NODE - NGINX REVERSE PROXY WITH MULTIPLE CONTAINERS</div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="data-container">
+      <div style={{borderRight: "2px solid"}}>
+        <h4>Node JS - APP 1</h4>
+      <button className="btn-dgn" style={{background: "#306c9e"}}onClick={getCountryData}> Get Country </button>
+      <ul>
+        {countryData.map((item,index) =>
+        <li key={index}>{item.country}- {item.state}</li>
+        )}
+      </ul>
+      </div>
+      <div>
+      <h4>Node JS - APP 2</h4>
+      <button className="btn-dgn" onClick={getDistrictData}> Get District </button>
+      <ul>
+        {districData.map((item,index) =>
+        <li key={index}>{item.state}- {item.district}</li>
+        )}
+      </ul>
+      </div>
+      </div>
       </header>
     </div>
   );
